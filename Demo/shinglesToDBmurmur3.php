1d28957495f053d16a6591be4038507687aa67ab
Alexey Tsimbalov <a.tsimbalov@gmail.com>
2016-10-28 23:32:04 +0300

    new way to walk source

--- Demo/shinglesToDBmurmur3.php
+++ Demo/shinglesToDBmurmur3.php
@@ -27,34 +27,38 @@ $logger->info('source: ' . $currentSource);
 $logger->info('source config: ' . print_r($sourceConfig, 1));
 
 
-$sqlCount = "SELECT min({$sourceConfig['input_text_id_field']}) as min_id, max({$sourceConfig['input_text_id_field']}) as max_id FROM {$sourceConfig['input_text_db']}";
+$sqlCount = "SELECT max({$sourceConfig['input_text_id_field']}) as max_id FROM {$sourceConfig['input_text_db']}";
 $limits   = $db->query($sqlCount, PDO::FETCH_OBJ)->fetchObject();
-$logger->info('sql: ' . $sqlCount);
 $logger->info('result: ' . print_r($limits));
 $stepSize = 100;
-$stepNof  = bcdiv(bcsub($limits->max_id, $limits->min_id), $stepSize);
-$logger->info('number of steps: ' . $stepNof);
+
 
 $sqlGetText = "SELECT {$sourceConfig['input_text_id_field']}, {$sourceConfig['input_text_field']} 
  FROM {$sourceConfig['input_text_db']} 
  WHERE 
- {$sourceConfig['input_text_id_field']} >= :id_from 
- AND {$sourceConfig['input_text_id_field']} < :id_to";
+ {$sourceConfig['input_text_id_field']} > :id_from 
+ ORDER BY {$sourceConfig['input_text_id_field']} ASC
+ LIMIT {$stepSize}";
 
 $sqlSaveShingle = "INSERT IGNORE INTO {$sourceConfig['output_text_db']} 
 SET text_id = :text_id, shingle_text = :shingle_text, shingle_hash = murmur_hash_v3(:shingle_text, 0)";
 
-for ($id = $limits->min_id; $id < $limits->max_id; $id = bcadd($id, $stepSize)) {
-    $logger->info('STEP ID: ' . $id . ' OF ' . $limits->max_id);
+$idFrom = 0;
+do {
+    $logger->info('STEP ID : ' . $idFrom . ' OF ' . $limits->max_id);
     $textListRequest = $db->prepare($sqlGetText);
-    $textListRequest->bindParam('id_from', $id);
-    $textListRequest->bindParam('id_to', bcadd($id, $stepSize));
+    $textListRequest->bindParam('id_from', $idFrom);
     $textListRequest->execute();
-    $textList = $textListRequest->fetchAll(PDO::FETCH_ASSOC);
+    $textList = $textListRequest->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_GROUP);
+    if(!empty($textList))
+    {
+        $textList = array_map('reset', $textList);
+    }
     $textNum  = 1;
     $db->beginTransaction();
 
-    foreach ($textList as $textMeta) {
+    foreach ($textList as $textId => $textMeta) {
+        $idFrom = $textId;
         $shingleList = $parser->parseText($textMeta['article_text']);
         $logger->info('TEXT: ' . $textNum);
         $textNum++;
@@ -66,4 +70,4 @@ for ($id = $limits->min_id; $id < $limits->max_id; $id = bcadd($id, $stepSize)) 
         }
     }
     $db->commit();
-}
+} while (count($textList) > 0);
