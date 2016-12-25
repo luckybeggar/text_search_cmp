8c0e0bcb47af475a596d188eeb99edc87553c67c
Alexey Tsimbalov <a.tsimbalov@gmail.com>
2016-12-25 18:21:32 +0300

    minor test hf changes

--- Demo/hash_func_test.php
+++ Demo/hash_func_test.php
@@ -11,10 +11,15 @@ require_once ('../Common/Logger.php');
 mb_internal_encoding('UTF-8');
 
 $logger = new Common_Logger(10);
-$options = array('hash_init_mode' => 'ready_prime', 'hash_prime' => '72057594037927931');
+//$options = array('hash_init_mode' => 'ready_prime', 'hash_prime' => '69605899126177831');
+$options = array('hash_init_mode' => 'number_of_bytes', 'hash_bytes' => '8');
+
+
 $hasher = new TSC_Hash_RK($options, null,  $logger);
 
 $logger->info(print_r($hasher->getHash('TEST GOOD TEST', null),1));
 $logger->info(print_r($hasher->getHash('GOOD TEST YES', 'TEST GOOD TEST'),1));
 $logger->info(print_r($hasher->getHash('GOOD TEST YES', null),1));
 
+$logger->info(print_r($hasher->getHash('ЗАИНТЕРЕСОВАЛО ДАННОЕ ПРЕДЛОЖЕНИЕ МОЖЕ  ТЕ ОЗНАКОМИТЬСЯ', null),1));
+$logger->info(print_r($hasher->getHash('ЕСЛИ ЗАИНТЕРЕСОВАЛО ДАННОЕ ПРЕДЛОЖЕНИЕ ОЗНАКОМИТЬСЯ', null),1));
